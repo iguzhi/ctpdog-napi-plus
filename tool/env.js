903699611ce0f085516807a1c843ac9280e053f6
weilee <383523223@qq.com>
2020-10-09 23:43:15 +0800

    重构

--- tool/env.js
+++ tool/env.js
@@ -3,7 +3,7 @@ const { getRoot } = require('bindings');
 
 if (process.platform === 'win32') {
   let root = getRoot(__filename);
-  let arch = process.arch;
+  let arch = process.arch === 'x64' ? 'x64' : '';
   let envpath = path.join(root, `api/tradeapi_win_${arch}`);
   console.log(envpath)
   if (process.env.PATH.indexOf(envpath) === -1) {
